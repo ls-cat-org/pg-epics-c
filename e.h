c0636e59770758064aa19f82c87770a44db075a5
Keith Brister <k-brister@northwestern.edu>
2011-09-21 20:29:17 +0000

    git-svn-id: file:///contrabass/repos/svn/e/trunk@2671 66ca1eeb-be3a-49d5-93d8-8bdade05efff

--- e.h
+++ e.h
@@ -10,8 +10,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <libpq-fe.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <signal.h>
 
 // For some reason epics uses fixed length strings
+// Sort of: epics strings are defined as a struct { unsigned length; char *pString}
+// but the dbr types (used exclusively by channel access) use epicsOldString defined as
+// typedef char epicsOldString[MAX_STRING_SIZE]
+//
+// I guess we are stuck with fixed length strings here
+// that we'll force to have the last byte be null
 //
 #define MAX_STRING_SIZE 40
 
@@ -38,10 +47,21 @@ typedef struct e_extended_message_header {
 // response packet
 //
 typedef struct e_response_struct {
-  struct sockaddr_in *peer;	// our peer
+  struct sockaddr_in peer;	// our peer
+  int sock;			// our socket
   int bufsize;			// number of bytes used in buf
   char *buf;			// our buffer, free when done
 } e_response_t;
+
+// reply queue
+//
+typedef struct e_reply_queue_struct {
+  struct e_reply_queue_struct *next;
+  struct sockaddr_in fromaddr;	// our from address to send reply to udp socket
+  int fromlen;			// length of from address
+  int reply_size;		// number of bytes in reply
+  char *reply_packet;		// calloc'ed reply packet
+} e_reply_queue_t;
 
 //
 // persistent socket information
@@ -49,11 +69,14 @@ typedef struct e_response_struct {
 typedef struct e_socks_buffer_struct {
   char *host_name;	// read from host name command
   char *user_name;	// read from user name command
+  int sock;		// our socket
   void *buf;		// input buffer
   int active;		// -1 when connection made; otherwise a count of active PV's served, 0 to close tcp connection
+  int events_on;	// 1 means send subscription updates, 0 means drop them in the bit bucket
   int bufsize;		// size of the buffer
   char *rbp;		// pointer to the next position in the buffer to read from
   char *wbp;		// pointer to the next position in the buffer to write to
+  e_reply_queue_t *reply_q;	// packets ready to send
 } e_socks_buffer_t;
 
 typedef struct e_dbr_size_struct {
@@ -61,3 +84,5 @@ typedef struct e_dbr_size_struct {
   int  dbr_struct_size;
   int  dbr_type_size;
 } e_dbr_size_t;
+
+
