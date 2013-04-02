179399867fa8f3d8eaa4760f567d0bbf91cc2ef2
Keith Brister <k-brister@northwestern.edu>
2013-04-02 14:11:01 +0000

    support for kvpairs and subscriptions (apparently added a long time ago)
    git-svn-id: file:///contrabass/repos/svn/e/branches/pre-ht@3108 66ca1eeb-be3a-49d5-93d8-8bdade05efff

--- e.h
+++ e.h
@@ -14,6 +14,7 @@
 #include <sys/time.h>
 #include <errno.h>
 #include <signal.h>
+#include <search.h>
 
 // For some reason epics uses fixed length strings
 // Sort of: epics strings are defined as a struct { unsigned length; char *pString}
@@ -80,6 +81,9 @@ typedef struct e_socks_buffer_struct {
   e_reply_queue_t *reply_q;	// packets ready to send
 } e_socks_buffer_t;
 
+//
+// dbr size info
+//
 typedef struct e_dbr_size_struct {
   char *dbr_name;
   int  dbr_struct_size;
@@ -87,3 +91,48 @@ typedef struct e_dbr_size_struct {
 } e_dbr_size_t;
 
 
+typedef struct e_subscription_struct {
+  struct e_subscription_struct *next;		// the next in the list
+  uint32_t dtype;				// the dbr type requested
+  uint32_t dcount;				// the count requested
+  uint32_t mask;				// our event mask  1=value changed, 2=log event, 4=alarm event
+  uint32_t subid;				// the client's subscription id
+} e_subscription_t;
+
+typedef struct e_channel_struct {
+  struct e_channel_struct *next;		// the next channel
+  char *hostname;				// where the client says its from
+  char *username;				// the user the client claims to represent
+  uint32_t version;				// the minor protocol version the clients wishes to use
+  uint32_t sock;				// the socket this channedl is created on
+  uint32_t cid;					// our channel id
+  e_subscription_t *subs;			// subscriptions using this channel
+} e_channel_t;
+
+typedef struct e_array_struct {
+  struct e_array_struct *next;			// the next element
+  struct e_kvpair_struct *name;			// the name kvpair
+  struct e_kvpair_struct *position;			// the position kvpair
+  uint32_t index;				// the index of this item
+} e_array_t;
+
+typedef struct e_kvpair_struct {
+  struct e_kvpair_struct *next;			// the next in the list
+  char *kvname;					// our name
+  char *sid_key;				// our alternate key
+  char *kvvalue;				// our value
+  uint32_t kvseq;				// the sequence number assigned by postgresql
+  uint32_t dbr_type;				// our "native" dbr type
+  uint32_t eepoch_secs;				// update time in seconds (seconds since 1/1/1990, the epics epoch)
+  uint32_t eepoch_nsecs;			// fraction part of the update time in nano seconds
+  struct e_kvpair_struct *high_limit;		// kv with our high limit or null if none
+  struct e_kvpair_struct *low_limit;		// kv with our low limit or null if none
+  struct e_kvpair_struct *high_limit_hit;	// kv that indicates our high limit has been hit
+  struct e_kvpair_struct *low_limit_hit;	// kv that indicates our low limit has been hit
+  struct e_kvpair_struct *prec;			// the number of decimal places to display
+  struct e_kvpair_struct *parent;		// if this is a high/low/prec field then this points to the parent kv
+  uint32_t sid;					// Our identifier for this entry
+  e_channel_t *chans;				// our subscribers
+  e_array_t   *array;				// perhaps we are really an array
+  uint32_t array_length;			// length of our perhaps array;
+} e_kvpair_t;
